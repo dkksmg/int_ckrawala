032fc39c2932a791037b95a5d2577ed5b20a4e4e
Dinkes Semarang <62316172+dkksmg@users.noreply.github.com>
2023-02-24 07:35:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ import re
 app = Flask(__name__)
   
   
-app.secret_key = 'xyzsdfg'
+# app.secret_key = 'xyzsdfg'
   
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'user'
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = ''
+# app.config['MYSQL_DB'] = 'user'
   
 mysql = MySQL(app)
   
